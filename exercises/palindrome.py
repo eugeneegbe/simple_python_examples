61dc3bd18bc9f562bc0f0230882eb046d409713a
Eugene233 <agboreugene@gmail.com>
2025-03-18 18:21:10 +0100

    Added custom palindrome function

---
+++ exercises/palindrome.py
@@ -0,0 +1,28 @@
+# write a python script to check if a string is a palindrom
+
+def check_palindrome(string):
+    size = len(string)
+    j = size - 1
+    i = 0
+    result = True
+    while i <= size / 2:
+        print(string[i], string[j])
+
+        if string[i] is not string[j]:
+            result = False
+        i += 1
+        j -= 1
+
+    return result
+
+
+string = 'saippuakivikauppias'
+print(check_palindrome(string))
+
+
+# A palindrome is a word spelled the same way in the reverse
+# For example "level"
+# iterate over the arrray and stop when in the middle and str[i] == str[j]
+# output check to be true
+# else it is false
+# Given the above example the output is True
